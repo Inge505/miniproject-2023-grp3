4604ad4d7ccb680593c7b5727babd1a60e34fa27
JulianPienaar <104741835+JulianPienaar@users.noreply.github.com>
2023-03-22 16:56:58 +0200

    Implemented lapse-post-component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { IonicModule } from '@ionic/angular';
+
 import { LapsePostComponent } from './lapse-post/lapse-post.component';
 
 @NgModule({
   declarations: [LapsePostComponent],
   imports: [
+    IonicModule,
     CommonModule
   ],
   exports: [LapsePostComponent]
