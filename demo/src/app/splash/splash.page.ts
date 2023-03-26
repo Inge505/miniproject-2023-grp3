0ed2af03a774e8f89f9aaf2ed2eff9a9471dba42
JulianPienaar <104741835+JulianPienaar@users.noreply.github.com>
2023-03-26 17:25:10 +0200

    Made default routing through the splash page

--- demo/src/app/splash/splash.page.ts
+++ demo/src/app/splash/splash.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-splash',
   templateUrl: './splash.page.html',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    setTimeout(()=>{
+      this.router.navigate(['/login']);
+    },
+    1500);
   }
 
 }
