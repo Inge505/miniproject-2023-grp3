99bdb01765c662712b72faeb9ce8884a71cf3563
Chris Mittendorf <u15092462@tuks.co.za>
2023-04-14 16:16:24 +0200

    updated repo with new values

--- libs/api/profiles/data-access/src/profiles.repository.ts
+++ libs/api/profiles/data-access/src/profiles.repository.ts
@@ -7,34 +7,34 @@ export class ProfilesRepository {
   async findOne(profile: IProfile) {
     return await admin
       .firestore()
-      .collection('profiles')
+      .collection('Users')
       .withConverter<IProfile>({
         fromFirestore: (snapshot) => {
           return snapshot.data() as IProfile;
         },
         toFirestore: (it: IProfile) => it,
       })
-      .doc(profile.userId)
+      .doc(profile.username)
       .get();
   }
 
   async createProfile(profile: IProfile) {
     // Remove password field if present
-    delete profile.accountDetails?.password;
+    //delete profile.accountDetails?.password;
     return await admin
       .firestore()
-      .collection('profiles')
-      .doc(profile.userId)
+      .collection('Users')
+      .doc(profile.username)
       .create(profile);
   }
 
   async updateProfile(profile: IProfile) {
     // Remove password field if present
-    delete profile.accountDetails?.password;
+    //delete profile.accountDetails?.password;
     return await admin
       .firestore()
-      .collection('profiles')
-      .doc(profile.userId)
+      .collection('Users')
+      .doc(profile.username)
       .set(profile, { merge: true });
   }
 }
