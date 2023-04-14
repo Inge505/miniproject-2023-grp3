cc07f924cedbf33f3df2c14e3d0765ee031998f5
Chris Mittendorf <u15092462@tuks.co.za>
2023-04-14 16:45:22 +0200

    updated post interface
    updated post interface to interact with firestore

--- libs/api/postss/util/src/interfaces/post.interface.ts
+++ libs/api/postss/util/src/interfaces/post.interface.ts
@@ -1,20 +1,20 @@
 import { Timestamp } from 'firebase-admin/firestore';
 import { Hashtag } from '../enums';
 import { IComment } from './comment.interface';
+import {IForSale} from "./for-sale.interface";
 
 export interface IPost {
-   postID: string;
-   createdBy: string;
-   likes: number; //fixed like left out
-   ownedBy: string | null | undefined;
-   buyerID?: string| null;
-   comments?: IComment[] | null;
-   createdAt?: Timestamp | null | undefined;
-   content?: string | null | undefined;
-   hashtag?: Hashtag | null |undefined;
-   caption? : string | null | undefined;
-   totalTime? : number | null | undefined;
-   ownerGainedTime?: number | null | undefined;
-   listing? : number | null | undefined
-   sold?: boolean
+  postID: string;
+  created?: Timestamp | null | undefined;
+  content?: string | null | undefined;
+  hashtag?: Hashtag | null |undefined;
+  caption? : string | null | undefined;
+  totalLikes?: number; //fixed like left out
+  likedBy?: string[] | null | undefined;
+  comments?: IComment[] | null;
+  ownerValue?: number | null | undefined;
+  ownedBy: string | null | undefined;
+  createdBy: string;
+  salePrice? : number | null | undefined;
+  forSale?: IForSale | null | undefined;
 }
