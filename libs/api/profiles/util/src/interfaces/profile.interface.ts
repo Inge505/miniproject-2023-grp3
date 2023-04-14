c4682cbadc81be7f813b1db10209601a7d4d6739
Chris Mittendorf <u15092462@tuks.co.za>
2023-04-14 15:39:58 +0200

    updated profile interface
    updated profile interface to have same variables as firestore

--- libs/api/profiles/util/src/interfaces/profile.interface.ts
+++ libs/api/profiles/util/src/interfaces/profile.interface.ts
@@ -5,14 +5,26 @@ import { IAddressDetails } from './address-details.interface';
 import { IContactDetails } from './contact-details.interface';
 import { IOccupationDetails } from './occupation-details.interface';
 import { IPersonalDetails } from './personal-details.interface';
+import { IUserSettings } from './user-settings.interface';
+import {IPost} from "@mp/api/postss/util";
 
 export interface IProfile {
-  userId: string;
-  accountDetails?: IAccountDetails | null | undefined;
-  personalDetails?: IPersonalDetails | null | undefined;
-  contactDetails?: IContactDetails | null | undefined;
-  addressDetails?: IAddressDetails | null | undefined;
-  occupationDetails?: IOccupationDetails | null | undefined;
-  status?: ProfileStatus | null | undefined;
-  created?: Timestamp | null | undefined;
+  username: string;
+  bio: string | null;
+  emailAddress: string;
+  posts?: IPost[] | null | undefined;
+  profilePicture?: string | null | undefined;
+  timeBalance?: number;
+  lastLogin?: Timestamp | null;
+  followers?: IProfile[] | null | undefined;
+  following?: IProfile[] | null | undefined;
+  userSettings?: IUserSettings[] | null;
 }
+
+  //accountDetails?: IAccountDetails | null | undefined;
+  //personalDetails?: IPersonalDetails | null | undefined;
+  //contactDetails?: IContactDetails | null | undefined;
+  //addressDetails?: IAddressDetails | null | undefined;
+  //occupationDetails?: IOccupationDetails | null | undefined;
+  //status?: ProfileStatus | null | undefined;
+  //created?: Timestamp | null | undefined;
